d6dc3da3940d5a2c14a43e6de2ca609721fc3f4e
Tina-Lightning <c.atherholt@gmail.com>
2023-02-03 14:50:33 -0500

    wrote the controller that gets the respose(the movies) from the User

--- movies/src/main/java/com/chrissyatherholtportfolio/movies/Movie.java
+++ movies/src/main/java/com/chrissyatherholtportfolio/movies/Movie.java
@@ -1,11 +1,21 @@
 package com.chrissyatherholtportfolio.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
     private ObjectId id;
     private String imdbId;
     private String title;
@@ -14,4 +24,6 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
 }
