23ec047c7b536bcaa5538fddbf24c7d0913bf65f
Tina-Lightning <c.atherholt@gmail.com>
2023-03-24 07:38:21 -0400

    started building react components

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -8,8 +11,6 @@ function App() {
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
-
-      console.log(response.data);
 
       setMovies(response.data);
     } catch (err) {
@@ -21,7 +22,15 @@ function App() {
     getMovies();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
